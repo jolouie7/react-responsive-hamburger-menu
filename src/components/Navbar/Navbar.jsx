1dfc4a875e6aa097c51ff9d631f883e7f533f13c
Your_Name <joseph161216@gmail.com>
2021-01-13 18:57:19 -0800

    Complete navbar with button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,34 +1,42 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { FaBars, FaTimes } from "react-icons/fa"
 import { IconContext } from "react-icons/lib"
+import { Button } from "../../globalStyles";
 import {
   Nav,
   NavbarContainer,
   NavLogo,
   NavIcon,
   HamburgerIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavItemBtn,
+  NavBtnLink,
 } from "./NavbarStyles";
 
 const Navbar = () => {
-  const [click, setClick] = useState(false)
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click)
 
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+  
+  useEffect(() => {
+    showButton();
+  }, []);
+
   return (
     <>
-      <IconContext.Provider value={{color: "#fff"}}>
+      <IconContext.Provider value={{ color: "#fff" }}>
         <Nav>
-          {/* <h1 className="navbar-logo">React</h1>
-        <div className="menu-icon">
-          icon
-        </div>
-        <ul>
-          <li><a href="#" className="nav-links">Home</a></li>
-          <li><a href="#" className="nav-links">Services</a></li>
-          <li><a href="#" className="nav-links">Products</a></li>
-          <li><a href="#" className="nav-links">Contact Us</a></li>
-          <li><a href="#" className="nav-links-mobile">Sign Up</a></li>
-        </ul> */}
           <NavbarContainer>
             <NavLogo>
               <NavIcon />
@@ -37,6 +45,28 @@ const Navbar = () => {
             <HamburgerIcon onClick={handleClick}>
               {click ? <FaTimes /> : <FaBars />}
             </HamburgerIcon>
+            <NavMenu click={click}>
+              <NavItem onClick={handleClick}>
+                <NavLinks to="/">Home</NavLinks>
+              </NavItem>
+              <NavItem onClick={handleClick}>
+                <NavLinks to="/services">Services</NavLinks>
+              </NavItem>
+              <NavItem onClick={handleClick}>
+                <NavLinks to="/products">Products</NavLinks>
+              </NavItem>
+              <NavItemBtn>
+                {button ? (
+                  <NavBtnLink to="/sign-up">
+                    <Button primary>SIGN UP</Button>
+                  </NavBtnLink>
+                ) : (
+                  <NavBtnLink to="/sign-up" onClick={handleClick}>
+                    <Button primary>SIGN UP</Button>
+                  </NavBtnLink>
+                )}
+              </NavItemBtn>
+            </NavMenu>
           </NavbarContainer>
         </Nav>
       </IconContext.Provider>
